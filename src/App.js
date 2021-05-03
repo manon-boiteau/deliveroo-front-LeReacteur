cd956288b4a5ed4f257b5d9ed46398c098be8789
Manon Boiteau <manonboiteau@mbp-de-manon.kpn>
2021-05-03 14:29:56 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
 
+/* Import Axios */
+import axios from "axios";
+
+/* Import useState() & useEffect() from React */
+import { useState, useEffect } from "react";
+
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/");
+        setData(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <header>
+        <img src="" alt="Deliveroo logo." />
+      </header>
     </div>
   );
 }
