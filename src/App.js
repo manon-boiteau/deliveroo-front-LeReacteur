8b7d18c41f38f42238f37ddb6c0670357be26dd5
Manon Boiteau <manonboiteau@mbp-de-manon.kpn>
2021-05-03 16:39:59 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -6,27 +6,95 @@ import axios from "axios";
 /* Import useState() & useEffect() from React */
 import { useState, useEffect } from "react";
 
+/* Import images */
+import logo from "./assets/img/deliveroo-logo.svg";
+
+/* Import Fontawsome */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(faStar);
+
 function App() {
   const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3100/");
+    setData(response.data);
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/");
-        setData(response.data);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
     fetchData();
   }, []);
 
-  return (
-    <div>
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
+    <>
       <header>
-        <img src="" alt="Deliveroo logo." />
+        <div className="wrapper">
+          <img src={logo} alt="Deliveroo logo." />
+        </div>
       </header>
-    </div>
+
+      <main>
+        <div className="description">
+          <div className="wrapper">
+            <div>
+              <h1>{data.restaurant.name}</h1>
+              <p>{data.restaurant.description}</p>
+            </div>
+            <div>
+              <img
+                src={data.restaurant.picture}
+                alt="Brunch Le Pain Quotidien."
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="wrapper categories">
+          {data.categories.map((elem, index) => {
+            return (
+              <div key={index}>
+                <h2>{elem.name}</h2>
+                <div className="bloc">
+                  {elem.meals.map((elem, index) => {
+                    return (
+                      <div key={index} className="bloc-meal">
+                        <div className="bloc-detail">
+                          <h3>{elem.title}</h3>
+                          <p>{elem.description}</p>
+                          <div>
+                            <span>{elem.price} €</span>
+                            {elem.popular ? (
+                              <FontAwesomeIcon
+                                icon="star"
+                                className="icon-star"
+                              />
+                            ) : null}
+                            <span className="star">
+                              {elem.popular ? "Populaire" : null}
+                            </span>
+                          </div>
+                        </div>
+                        <div>
+                          {elem.picture ? (
+                            <img src={elem.picture} alt={elem.title} />
+                          ) : null}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </>
   );
 }
 
