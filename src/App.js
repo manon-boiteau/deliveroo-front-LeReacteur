a81955d3254a793c951d1ce05d70a3fd13e1e9dd
Manon Boiteau <manonboiteau@mbp-de-manon.kpn>
2021-05-04 10:47:37 +0200

    replace index by id

--- src/App.js
+++ src/App.js
@@ -60,38 +60,42 @@ function App() {
         <div className="wrapper categories">
           {data.categories.map((elem, index) => {
             return (
-              <div key={index}>
-                <h2>{elem.name}</h2>
-                <div className="bloc">
-                  {elem.meals.map((elem, index) => {
-                    return (
-                      <div key={index} className="bloc-meal">
-                        <div className="bloc-detail">
-                          {elem.title ? <h3>{elem.title}</h3> : null}
-                          {elem.description ? <p>{elem.description}</p> : null}
-                          <div>
-                            <span>{elem.price} €</span>
-                            {elem.popular ? (
-                              <FontAwesomeIcon
-                                icon="star"
-                                className="icon-star"
-                              />
+              elem.meals.length > 0 && (
+                <div key={index}>
+                  <h2>{elem.name}</h2>
+                  <div className="bloc">
+                    {elem.meals.map((elem, index) => {
+                      return (
+                        <div key={elem.id} className="bloc-meal">
+                          <div className="bloc-detail">
+                            {elem.title ? <h3>{elem.title}</h3> : null}
+                            {elem.description ? (
+                              <p>{elem.description}</p>
                             ) : null}
-                            <span className="star">
-                              {elem.popular ? "Populaire" : null}
-                            </span>
+                            <div>
+                              <span>{elem.price} €</span>
+                              {elem.popular ? (
+                                <FontAwesomeIcon
+                                  icon="star"
+                                  className="icon-star"
+                                />
+                              ) : null}
+                              <span className="star">
+                                {elem.popular ? "Populaire" : null}
+                              </span>
+                            </div>
+                          </div>
+                          <div>
+                            {elem.picture ? (
+                              <img src={elem.picture} alt={elem.title} />
+                            ) : null}
                           </div>
                         </div>
-                        <div>
-                          {elem.picture ? (
-                            <img src={elem.picture} alt={elem.title} />
-                          ) : null}
-                        </div>
-                      </div>
-                    );
-                  })}
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
+              )
             );
           })}
         </div>
